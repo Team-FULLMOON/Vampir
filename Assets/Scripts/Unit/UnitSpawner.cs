2284c0ad50e418ec960ac883d1ddf1824f22e5a5
rhtjdwns <ib041615@gmail.com>
2024-03-16 16:22:57 +0900

    유닛이동 구현

---
+++ Assets/Scripts/Unit/UnitSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject unitPrefab;
+    [SerializeField] int maxUnitCount;
+
+    private Vector2 minSize = new Vector2(-22, -22);
+    private Vector2 maxSize = new Vector2(22, 22);
+
+    public List<UnitController> SpawnUnits()
+    {
+        List<UnitController> unitList = new List<UnitController>(maxUnitCount);
+
+        for (int i = 0; i < maxUnitCount; ++i)
+        {
+            Vector3 position = new Vector3(Random.Range(minSize.x, maxSize.x), 1, Random.Range(minSize.y, maxSize.y));
+
+            GameObject clone = ObjectPoolManager.SpawnObject(unitPrefab, position, Quaternion.identity);
+            UnitController unit = clone.GetComponent<UnitController>();
+
+            unitList.Add(unit);
+        }
+
+        return unitList;
+    }
+}
