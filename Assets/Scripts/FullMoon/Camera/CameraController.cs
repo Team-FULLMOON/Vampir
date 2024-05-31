665f8994b28745929a64ccceb79bce6ea0b6ca0e
Lee SJ <re.elif@icloud.com>
2024-05-31 19:57:19 +0900

    UI 수정

--- Assets/Scripts/FullMoon/Camera/CameraController.cs
+++ Assets/Scripts/FullMoon/Camera/CameraController.cs
@@ -341,7 +341,6 @@ namespace FullMoon.Camera
                 unit.Deselect();
             }
 
-            MainUIController.Instance.OffButton();
             selectedUnitList.Clear();
         }
 
--- Assets/Scripts/FullMoon/UI/MainUIController.cs
+++ Assets/Scripts/FullMoon/UI/MainUIController.cs
@@ -12,155 +12,24 @@ namespace FullMoon.UI
     public class MainUIController : ComponentSingleton<MainUIController>
     {
         [SerializeField] private UnitControlData unitControlData;
-
-        public ProgressBar ManaProgressBar { get; private set; }
-        public TextElement ManaProgressText { get; private set; }
-        
-        public Button UnitExpandButton { get; private set; }
-        public TextElement UnitLimitText { get; private set; }
         
         public Button RetryButton { get; private set; }
         
         public VisualElement PhaseElement { get; private set; }
         public TextElement PhaseText { get; private set; }
         public TextElement PhaseDetailText { get; private set; }
-
-        #region Shortcuts
-
-        public Button ShortcutMoveButton { get; private set; }
-        public Button ShortcutStopButton { get; private set; }
-        public Button ShortcutAttackMoveButton { get; private set; }
-        public Button ShortcutHoldButton { get; private set; }
-        public Button ShortcutCancelButton { get; private set; }
-
-        public ReactiveProperty<bool> canMove;
-        public ReactiveProperty<bool> canAttack;
-
-        #endregion
-        
-        public int ManaValue => (int)ManaProgressBar.value;
-        public int UnitLimitValue { get; private set; }
-        public int CurrentUnitValue { get; private set; }
         
         private void OnEnable()
         {
             VisualElement root = GetComponent<UIDocument>().rootVisualElement;
             
-            ManaProgressBar = root.Q<ProgressBar>("ManaProgressBar");
-            ManaProgressText = root.Q<TextElement>("ManaProgressText");
-            SetMaxMana(unitControlData.InitMaxMana);
-            
-            UnitExpandButton = root.Q<Button>("UnitExpandButton");
-            UnitLimitText = root.Q<TextElement>("UnitLimitText");
-            UnitExpandButton.RegisterCallback<ClickEvent>(ExpandUnit);
-            SetUnitLimit(unitControlData.InitUnitLimit);
-            
             RetryButton = root.Q<Button>("RetryButton");
             RetryButton.RegisterCallback<ClickEvent>(Retry);
-            AddMana(30);
-
-            ShortcutMoveButton = root.Q<Button>("ShortcutMoveButton");
-            ShortcutStopButton = root.Q<Button>("ShortcutStopButton");
-            ShortcutAttackMoveButton = root.Q<Button>("ShortcutAttackMoveButton");
-            ShortcutHoldButton = root.Q<Button>("ShortcutHoldButton");
-            ShortcutCancelButton = root.Q<Button>("ShortcutCancelButton");
-            
-            canMove = new ReactiveProperty<bool>(false);
-            canAttack = new ReactiveProperty<bool>(false);
-
-            canMove.Subscribe(move =>
-            {
-                ShortcutMoveButton.SetEnabled(move);
-                ShortcutStopButton.SetEnabled(!move);
-                ShortcutAttackMoveButton.SetEnabled(!move);
-                ShortcutHoldButton.SetEnabled(!move);
-                ShortcutCancelButton.SetEnabled(move);
-                if (!move)
-                {
-                    ShortcutMoveButton.SetEnabled(true);
-                }
-            });
-
-            canAttack.Subscribe(attack =>
-            {
-                ShortcutMoveButton.SetEnabled(!attack);
-                ShortcutStopButton.SetEnabled(!attack);
-                ShortcutAttackMoveButton.SetEnabled(attack);
-                ShortcutHoldButton.SetEnabled(!attack);
-                ShortcutCancelButton.SetEnabled(attack);
-                if (!attack)
-                {
-                    ShortcutAttackMoveButton.SetEnabled(true);
-                }
-            });
-
-            OffButton();
             
             PhaseElement = root.Q<VisualElement>("Phase");
             PhaseText = root.Q<TextElement>("PhaseText");
             PhaseDetailText = root.Q<TextElement>("PhaseDetailText");
             PhaseElement.SetVisible(false);
-        }
-
-        public void MainUnitActiveButton()
-        {
-            ShortcutMoveButton.SetEnabled(true);
-            ShortcutStopButton.SetEnabled(true);
-            ShortcutAttackMoveButton.SetEnabled(true);
-            ShortcutHoldButton.SetEnabled(true);
-        }
-
-        public void UnitActiveButton()
-        {
-            ShortcutMoveButton.SetEnabled(true);
-            ShortcutStopButton.SetEnabled(true);
-            ShortcutAttackMoveButton.SetEnabled(true);
-            ShortcutHoldButton.SetEnabled(true);
-        }
-
-        public void OffButton()
-        {
-            ShortcutMoveButton.SetEnabled(false);
-            ShortcutStopButton.SetEnabled(false);
-            ShortcutAttackMoveButton.SetEnabled(false);
-            ShortcutHoldButton.SetEnabled(false);
-            ShortcutCancelButton.SetEnabled(false);
-        }
-
-        public void AddMana(int value)
-        {
-            ManaProgressBar.value = Mathf.Clamp(ManaProgressBar.value + value, 0f, ManaProgressBar.highValue);
-            ManaProgressText.text = $"{ManaProgressBar.value} / {ManaProgressBar.highValue}";
-        }
-        
-        public void AddUnit(int value)
-        {
-            CurrentUnitValue = Mathf.Clamp(CurrentUnitValue + value, 0, Int32.MaxValue);
-            UnitLimitText.text = $"{CurrentUnitValue} / {UnitLimitValue}";
-            UnitLimitText.style.color = new StyleColor(CurrentUnitValue >= UnitLimitValue ? Color.red : Color.white);
-        }
-        
-        private void SetMaxMana(int value)
-        {
-            ManaProgressBar.highValue = value;
-            ManaProgressText.text = $"{ManaProgressBar.value} / {ManaProgressBar.highValue}";
-        }
-
-        private void SetUnitLimit(int value)
-        {
-            UnitLimitValue = value;
-            UnitLimitText.text = $"{CurrentUnitValue} / {UnitLimitValue}";
-            UnitLimitText.style.color = new StyleColor(CurrentUnitValue >= UnitLimitValue ? Color.red : Color.white);
-        }
-        
-        private void ExpandUnit(ClickEvent evt)
-        {
-            if (ManaProgressBar.value < unitControlData.UnitLimitExpandCost)
-            {
-                return;
-            }
-            AddMana(-unitControlData.UnitLimitExpandCost);
-            SetUnitLimit(UnitLimitValue + unitControlData.UnitLimitExpandValue);
         }
         
         private void Retry(ClickEvent evt)
